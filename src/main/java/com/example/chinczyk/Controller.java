0bfc19275bace6c1fd4cee9843ae4972edcb9be7
Darek07 <darektsydik@gmail.com>
2022-06-02 02:25:57 +0200

    Refactor names in Controller.java

--- src/main/java/com/example/chinczyk/Controller.java
+++ src/main/java/com/example/chinczyk/Controller.java
@@ -12,110 +12,110 @@ import com.example.chinczyk.Pawn.PawnColor;
 
 public class Controller implements Initializable {
 
-    @FXML private Circle blue_domek1;
-    @FXML private Circle blue_domek2;
-    @FXML private Circle blue_domek3;
-    @FXML private Circle blue_domek4;
+    @FXML private Circle blueHome1;
+    @FXML private Circle blueHome2;
+    @FXML private Circle blueHome3;
+    @FXML private Circle blueHome4;
 
-    @FXML private Circle blue_schowek2;
-    @FXML private Circle blue_schowek3;
-    @FXML private Circle blue_schowek4;
-    @FXML private Circle blue_schwek1;
+    @FXML private Circle blueYard1;
+    @FXML private Circle blueYard2;
+    @FXML private Circle blueYard3;
+    @FXML private Circle blueYard4;
 
-    @FXML private Circle blue_start;
+    @FXML private Circle blueStart;
 
-    @FXML private Circle green_domek1;
-    @FXML private Circle green_domek2;
-    @FXML private Circle green_domek3;
-    @FXML private Circle green_domek4;
+    @FXML private Circle greenHome1;
+    @FXML private Circle greenHome2;
+    @FXML private Circle greenHome3;
+    @FXML private Circle greenHome4;
 
-    @FXML private Circle green_schowek1;
-    @FXML private Circle green_schowek2;
-    @FXML private Circle green_schowek3;
-    @FXML private Circle green_schowek4;
+    @FXML private Circle greenYard1;
+    @FXML private Circle greenYard2;
+    @FXML private Circle greenYard3;
+    @FXML private Circle greenYard4;
 
-    @FXML private Circle green_start;
+    @FXML private Circle greenStart;
 
-    @FXML private Circle red_domek1;
-    @FXML private Circle red_domek2;
-    @FXML private Circle red_domek3;
-    @FXML private Circle red_domek4;
+    @FXML private Circle redHome1;
+    @FXML private Circle redHome2;
+    @FXML private Circle redHome3;
+    @FXML private Circle redHome4;
 
-    @FXML private Circle red_schowek1;
-    @FXML private Circle red_schowek2;
-    @FXML private Circle red_schowek3;
-    @FXML private Circle red_schowek4;
+    @FXML private Circle redYard1;
+    @FXML private Circle redYard2;
+    @FXML private Circle redYard3;
+    @FXML private Circle redYard4;
 
-    @FXML private Circle red_start;
+    @FXML private Circle redStart;
 
-    @FXML private Circle yellow_domek1;
-    @FXML private Circle yellow_domek2;
-    @FXML private Circle yellow_domek3;
-    @FXML private Circle yellow_domek4;
+    @FXML private Circle yellowHome1;
+    @FXML private Circle yellowHome2;
+    @FXML private Circle yellowHome3;
+    @FXML private Circle yellowHome4;
 
-    @FXML private Circle yellow_schowek1;
-    @FXML private Circle yellow_schowek2;
-    @FXML private Circle yellow_schowek3;
-    @FXML private Circle yellow_schowek4;
+    @FXML private Circle yellowYard1;
+    @FXML private Circle yellowYard2;
+    @FXML private Circle yellowYard3;
+    @FXML private Circle yellowYard4;
 
-    @FXML private Circle yellow_start;
+    @FXML private Circle yellowStart;
 
-    @FXML private Pawn blue_pawn1;
-    @FXML private Pawn blue_pawn2;
-    @FXML private Pawn blue_pawn3;
-    @FXML private Pawn blue_pawn4;
+    @FXML private Pawn bluePawn1;
+    @FXML private Pawn bluePawn2;
+    @FXML private Pawn bluePawn3;
+    @FXML private Pawn bluePawn4;
 
-    @FXML private Pawn green_pawn1;
-    @FXML private Pawn green_pawn2;
-    @FXML private Pawn green_pawn3;
-    @FXML private Pawn green_pawn4;
+    @FXML private Pawn greenPawn1;
+    @FXML private Pawn greenPawn2;
+    @FXML private Pawn greenPawn3;
+    @FXML private Pawn greenPawn4;
 
-    @FXML private Pawn red_pawn1;
-    @FXML private Pawn red_pawn2;
-    @FXML private Pawn red_pawn3;
-    @FXML private Pawn red_pawn4;
+    @FXML private Pawn redPawn1;
+    @FXML private Pawn redPawn2;
+    @FXML private Pawn redPawn3;
+    @FXML private Pawn redPawn4;
 
-    @FXML private Pawn yellow_pawn1;
-    @FXML private Pawn yellow_pawn2;
-    @FXML private Pawn yellow_pawn3;
-    @FXML private Pawn yellow_pawn4;
+    @FXML private Pawn yellowPawn1;
+    @FXML private Pawn yellowPawn2;
+    @FXML private Pawn yellowPawn3;
+    @FXML private Pawn yellowPawn4;
 
     private static final Map<PawnColor, Circle> startCells = new HashMap<>(4);
     private static final Set<Circle> homeCells = new HashSet<>(16);
 
-    private final Set<Pawn> blue_pionki = new HashSet<>(4);
-    private final Set<Pawn> green_pionki = new HashSet<>(4);
-    private final Set<Pawn> red_pionki = new HashSet<>(4);
-    private final Set<Pawn> yellow_pionki = new HashSet<>(4);
+    private final Set<Pawn> bluePawns = new HashSet<>(4);
+    private final Set<Pawn> greenPawns = new HashSet<>(4);
+    private final Set<Pawn> redPawns = new HashSet<>(4);
+    private final Set<Pawn> yellowPawns = new HashSet<>(4);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        startCells.put(PawnColor.BLUE, blue_start);
-        startCells.put(PawnColor.GREEN, green_start);
-        startCells.put(PawnColor.RED, red_start);
-        startCells.put(PawnColor.YELLOW, yellow_start);
+        startCells.put(PawnColor.BLUE, blueStart);
+        startCells.put(PawnColor.GREEN, greenStart);
+        startCells.put(PawnColor.RED, redStart);
+        startCells.put(PawnColor.YELLOW, yellowStart);
         Collections.addAll(homeCells,
-                blue_domek1,blue_domek2,blue_domek3,blue_domek4,
-                green_domek1,green_domek2,green_domek3,green_domek4,
-                red_domek1,red_domek2,red_domek3,red_domek4,
-                yellow_domek1,yellow_domek2,yellow_domek3,yellow_domek4);
+                blueHome1, blueHome2, blueHome3, blueHome4,
+                greenHome1, greenHome2, greenHome3, greenHome4,
+                redHome1, redHome2, redHome3, redHome4,
+                yellowHome1, yellowHome2, yellowHome3, yellowHome4);
 
-        Collections.addAll(blue_pionki, blue_pawn1, blue_pawn2, blue_pawn3, blue_pawn4);
-        Collections.addAll(green_pionki, green_pawn1, green_pawn2, green_pawn3, green_pawn4);
-        Collections.addAll(red_pionki, red_pawn1, red_pawn2, red_pawn3, red_pawn4);
-        Collections.addAll(yellow_pionki, yellow_pawn1, yellow_pawn2, yellow_pawn3, yellow_pawn4);
+        Collections.addAll(bluePawns, bluePawn1, bluePawn2, bluePawn3, bluePawn4);
+        Collections.addAll(greenPawns, greenPawn1, greenPawn2, greenPawn3, greenPawn4);
+        Collections.addAll(redPawns, redPawn1, redPawn2, redPawn3, redPawn4);
+        Collections.addAll(yellowPawns, yellowPawn1, yellowPawn2, yellowPawn3, yellowPawn4);
 
-        Stream.of(blue_pionki, green_pionki, red_pionki, yellow_pionki)
+        Stream.of(bluePawns, greenPawns, redPawns, yellowPawns)
                 .flatMap(Collection::stream)
                 .forEach(Pawn::initialize);
     }
 
     public Set<Pawn> getPawnsByType(PawnColor type) {
         return switch (type) {
-            case BLUE -> blue_pionki;
-            case GREEN -> green_pionki;
-            case RED -> red_pionki;
-            case YELLOW -> yellow_pionki;
+            case BLUE -> bluePawns;
+            case GREEN -> greenPawns;
+            case RED -> redPawns;
+            case YELLOW -> yellowPawns;
         };
     }
 
